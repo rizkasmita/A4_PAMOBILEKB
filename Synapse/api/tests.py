19a05603a54e319ef91e551f24595ef28ac48d57
R <123613447+rizkasmita@users.noreply.github.com>
2024-12-07 00:22:50 +0800

    Delete Synapse directory

--- Synapse/api/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from .models import Image
-
-class ImageModelTest(TestCase):
-    def test_string_representation(self):
-        image = Image(name="Test Image")
-        self.assertEqual(str(image), "Test Image")
